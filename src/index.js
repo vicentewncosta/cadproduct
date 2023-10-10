8be051fcd55a431e16adafd91619a7478fc04638
Liggia <liggiaslima@gmail.com>
2023-10-10 00:45:24 -0300

    Criação da página home

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'bootswatch/dist/sketchy/bootstrap.min.css';
+import 'bootswatch/dist/vapor/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
---
+++ src/views/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home () {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-3">Bem-vindo(a)!👋🏾</h1>
+            <p className="lead">
+                Sistema em testes.
+            </p>
+            <hr className="my-4" />
+            <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a>
+            </p>
+        </div>
+    )    
+}
+export default Home;
